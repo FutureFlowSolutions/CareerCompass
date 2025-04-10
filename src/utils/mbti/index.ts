e56c6c80c609a54edc9fa1cbfc0298ae899fb424
CoderTemperror <futureflowpos@gmail.com>
2025-04-10 18:28:51 +0530

    idk

--- src/utils/mbti/index.ts
+++
@@ -1,7 +0,0 @@
-
-// Re-export everything from the individual files
-export * from './types';
-export * from './questions';
-export * from './personalityDescriptions';
-export * from './calculator';
-export * from './suggestedPrompts';
--- src/utils/mbtiCalculator.ts
+++
@@ -1,4 +0,0 @@
-
-// This file is kept for backward compatibility
-// It re-exports all the MBTI functionality from the reorganized files
-export * from './mbti';
